17700b7710dcb44e58c4ec3ef553d905ad4173f8
NicoYang <ohgod723@gmail.com>
2013-10-13 04:21:51 -0700

    test for parameter

--- Test.py
+++ Test.py
@@ -4,9 +4,9 @@ def main():
 	print 'finally'
 	str1='this is test!'
 	print str1
-	print sys.argv[1]
-	print sys.argv[2]
-	print sys.argv[3]
+	print $WORKSPACE
+	'''print sys.argv[2]
+	print sys.argv[3]'''
 		
 if __name__ == "__main__":
     main()
