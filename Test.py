0445383240587a57419768de46645c855354e434
NicoYang <ohgod723@gmail.com>
2013-10-13 03:50:25 -0700

    test for parameter

--- Test.py
+++ Test.py
@@ -1,11 +1,12 @@
 class Test:
-def main(workspace,Jenkins_home,Jenkins_url):
-	print 'finally'
-	str1='this is test!'
-	print str1
-	print workspace
-	print Jenkins_home
-	print Jenkins_url
+	def main(workspace,Jenkins_home,Jenkins_url):
+		print 'finally'
+		str1='this is test!'
+		print str1
+		print workspace
+		print Jenkins_home
+		print Jenkins_url
 	
 if __name__ == "__main__":
-   main(workspace,Jenkins_home,Jenkins_url)
+	m = Main()
+    m.main(workspace,Jenkins_home,Jenkins_url)
