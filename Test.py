dc408d14c3bab0feb6c7a2c384f2ce881cf1981e
NicoYang <ohgod723@gmail.com>
2013-10-13 04:15:33 -0700

    test for parameter

--- Test.py
+++ Test.py
@@ -1,9 +1,12 @@
-class Test:
-	def main(workspace,Jenkins_home,Jenkins_url):
-		print 'finally'
-		str1='this is test!'
-		print str1
-		print workspace
-		print Jenkins_home
-		print Jenkins_url
-	
+import sys
+
+def main():
+	print 'finally'
+	str1='this is test!'
+	print str1
+	print sys.argv[1]
+	print sys.argv[2]
+	print sys.argv[3]
+		
+if __name__ == "__main__":
+    main()
